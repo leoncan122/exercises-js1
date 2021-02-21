aa0b59626547774b79c11b5b7ea10b1cf491c775
leoncan122 <leon.can122@gmail.com>
2021-02-21 17:44:00 +0100

    exercise.js

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -11,12 +11,12 @@ console.log(Math.round(num_students));
 
 function per_class(students, mentors) {
     per_total = 100
-    total_students = 19
+    total_students = 14
     per_students = (students * per_total) / total_students;
     per_mentors = (mentors * per_total) / total_students;
     return "percent of students: " + Math.round(per_students) + "     percent of mentors: " + Math.round(per_mentors);
 }
 
-console.log(per_class(14, 5));
+console.log(per_class(9, 5));
 
 
